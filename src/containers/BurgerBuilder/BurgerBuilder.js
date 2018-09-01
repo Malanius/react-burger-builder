007b6909e9f6c3fb75033c8cbe556d52ea01d76a
Malanius <malanius@seznam.cz>
2018-09-01 12:15:43 +0200

    Create a backdrop for order summary modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -79,6 +79,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: true});
     }
 
+    purchaseCancelledHandler = () => {
+        this.setState({purchasing: false});
+    }
+
     render() {
         const disableInfo = {
             ...this.state.ingredients
@@ -90,7 +94,7 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelledHandler}>
                     <OrderSummary ingredients={this.state.ingredients} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
