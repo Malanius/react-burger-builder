43b120c16586f0b6d1a619fae0174b8362b3dcc9
Malanius <malanius@seznam.cz>
2018-09-06 18:39:30 +0200

    Move components so that it reflects what it really is

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,7 +4,7 @@ import classes from './SideDrawer.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/Auxi';
+import Aux from '../../../hoc/Auxi/Auxi';
 
 export default (props) => {
     let attachedClasses = [classes.SideDrawer, classes.Close];
