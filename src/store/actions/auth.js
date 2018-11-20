c756313c278271a873b989868aad25b5ff609c30
Malanius <malanius@seznam.cz>
2018-11-20 18:34:23 +0100

    Logout after token expires

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,21 @@ export const authFail = (error) => {
     }
 }
 
+export const logOut = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    }
+}
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logOut());
+        }, expirationTime * 1000);
+    }
+}
+
+
 export const auth = (email, password, isSignUp) => {
     return dispatch => {
         dispatch(authStart());
@@ -39,6 +54,7 @@ export const auth = (email, password, isSignUp) => {
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
             })
             .catch(err => {
                 console.log(err);
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,5 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
-import {updateObject} from '../util';
+import { updateObject } from '../util';
 
 const initialState = {
     token: null,
@@ -28,11 +28,19 @@ const authFail = (state, action) => {
     })
 }
 
+const authLogout = (state, action) => {
+    return updateObject(state, {
+        token: null,
+        userId: null
+    })
+}
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.AUTH_START: return authStart(state, action);
         case actionTypes.AUTH_SUCCESS: return authSucces(state, action);
         case actionTypes.AUTH_FAIL: return authFail(state, action);
+        case actionTypes.AUTH_LOGOUT: return authLogout(state, action);
         default:
             return state;
 
