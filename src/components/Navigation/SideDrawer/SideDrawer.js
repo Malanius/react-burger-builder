abddbbc37631e11e92eb8b89bd82df15f4e428b1
Malanius <malanius@seznam.cz>
2018-11-26 19:21:20 +0100

    Close side drawen on click

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -8,19 +8,19 @@ import Aux from '../../../hoc/Auxi/Auxi';
 
 export default (props) => {
     let attachedClasses = [classes.SideDrawer, classes.Close];
-    if (props.open){
+    if (props.open) {
         attachedClasses = [classes.SideDrawer, classes.Open];
     }
 
     return (
         <Aux>
             <Backdrop show={props.open} clicked={props.closed} />
-            <div className={attachedClasses.join(' ')}>
+            <div className={attachedClasses.join(' ')} onClick={props.closed}>
                 <div className={classes.Logo}>
                     <Logo />
                 </div>
                 <nav>
-                    <NavigationItems isAuthenticated={props.isAuth}/>
+                    <NavigationItems isAuthenticated={props.isAuth} />
                 </nav>
             </div>
         </Aux>
