03edf4554637fa923db7ce97a4813777ce4fcc15
Malanius <malanius@seznam.cz>
2018-12-08 15:20:05 +0100

    Hook up the logout saga

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,8 +1,8 @@
 import { put } from 'redux-saga/effects';
 
-import actionTypes from '../actions/actionTypes';
+import * as actionTypes from '../actions/actionTypes';
 
-function* logout(action) {
+export function* logoutSaga(action) {
     yield localStorage.removeItem('token');
     yield localStorage.removeItem('expirationDate');
     yield localStorage.removeItem('userId');
